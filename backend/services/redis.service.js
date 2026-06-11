a4815e4ab8a97d2828b3a034fd4e0f03b266b192
amanchouhan01 <amanc0508@gmail.com>
2026-06-11 23:06:15 +0530

    Updated redis.service.js

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -1,15 +1,18 @@
 import Redis from "ioredis";
 
-
 const redisClient = new Redis({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD,
+    tls: {}
 });
 
-
-redisClient.on('connect', () =>{
+redisClient.on('connect', () => {
     console.log('Redis connected');
+});
+
+redisClient.on('error', (err) => {
+    console.log('Redis error:', err);
 });
 
 export default redisClient;
